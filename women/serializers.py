7281609fb3823411084dc6cdc81fcabc938678b9
Никита <ilin.15052003@gmail.com>
2025-02-21 08:56:55 +0300

    Start in DRF (2)

--- women/serializers.py
+++ women/serializers.py
@@ -1,9 +1,35 @@
+import io
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+# class WomeModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = WomeModel('Angelina Jolie', 'Content: Angelina Jolie')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}')
+#     data = JSONRenderer().parse(stream)
+#     serializers = WomenSerializer(data=data)
+#     serializers.is_valid()
+#     print(serializers.validated_data)
