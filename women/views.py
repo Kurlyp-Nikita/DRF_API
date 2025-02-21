a9a1d4fff8ed06eb42e64c7bc2411dd5e5a097f3
Никита <ilin.15052003@gmail.com>
2025-02-21 10:03:16 +0300

    Start in DRF (3)

--- women/views.py
+++ women/views.py
@@ -15,13 +15,38 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Objects dose not axists"})
+
+        serialisers = WomenSerializer(data=request.data, instance=instance)
+        serialisers.is_valid(raise_exception=True)
+        serialisers.save()
+        return Response({'post': serialisers.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method DELETE not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+            instance.delete()
+        except:
+            return Response({"error": "Objects dose not axists"})
+        return Response({"post": "delete post " + str(pk)})
+
+
 
 
 # class WomenAPIView(generics.ListAPIView):
