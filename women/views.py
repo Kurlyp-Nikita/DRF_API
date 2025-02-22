7ec4bfda04c3390e2feef7c11fb3afa6908925c0
Никита <ilin.15052003@gmail.com>
2025-02-22 10:17:14 +0300

    Start in DRF (5)

--- women/views.py
+++ women/views.py
@@ -11,49 +11,70 @@ from women.serializers import WomenSerializer
 # Предоставляет обработчики методов get и post.
 # Расширяет: GenericAPIView, ListModelMixin, CreateModelMixin
 
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()  # данные которые возвращаются ао запросу
     serializer_class = WomenSerializer  # класс serializer_class, который обрабатывает queryset
 
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        lst = Women.objects.all()
-        return Response({'posts': WomenSerializer(lst, many=True).data})
+# UpdateAPIView. Используется только для конечных точек обновления для одного экземпляра модели.
+# Предоставляет обработчики методов put и patch.
+# Расширяет: GenericAPIView, UpdateModelMixin
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response({'post': serializer.data})
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()  # данные которые возвращаются ао запросу
+    serializer_class = WomenSerializer  # класс serializer_class, который обрабатывает queryset
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
 
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Objects dose not axists"})
+# RetrieveUpdateDestroyAPIView. Используется для конечных точек чтения, записи и удаления для представления одного экземпляра модели.
+# Предоставляет обработчики методов get, put, patch и delete.
+# Расширяет: GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
-        serialisers = WomenSerializer(data=request.data, instance=instance)
-        serialisers.is_valid(raise_exception=True)
-        serialisers.save()
-        return Response({'post': serialisers.data})
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method DELETE not allowed"})
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()  # данные которые возвращаются ао запросу
+    serializer_class = WomenSerializer  # класс serializer_class, который обрабатывает queryset
 
-        try:
-            instance = Women.objects.get(pk=pk)
-            instance.delete()
-        except:
-            return Response({"error": "Objects dose not axists"})
-        return Response({"post": "delete post " + str(pk)})
+
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         lst = Women.objects.all()
+#         return Response({'posts': WomenSerializer(lst, many=True).data})
+#
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#
+#         return Response({'post': serializer.data})
+#
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Objects dose not axists"})
+#
+#         serialisers = WomenSerializer(data=request.data, instance=instance)
+#         serialisers.is_valid(raise_exception=True)
+#         serialisers.save()
+#         return Response({'post': serialisers.data})
+#
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method DELETE not allowed"})
+#
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#             instance.delete()
+#         except:
+#             return Response({"error": "Objects dose not axists"})
+#         return Response({"post": "delete post " + str(pk)})
 
 
 
